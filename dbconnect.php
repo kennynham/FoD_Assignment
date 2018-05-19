e82216327cf5e9db730df4d308b11ef6a8a2fe57
kennynham <knham1@csu.fullerton.edu>
2018-05-18 20:31:06 -0700

    Removed index.php. Changed db variable name in dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,9 +2,9 @@
     $SERVER_NAME = 'localhost';
     $USER_NAME   = 'root';
     $PASSWORD    = 'root';
-    $DB_NAME     = 'fod_assignment';
+    $DB          = 'fod_assignment';
 
-    $connection = new mysqli($SERVER_NAME, $USER_NAME, $PASSWORD, $DB_NAME);
+    $connection = new mysqli($SERVER_NAME, $USER_NAME, $PASSWORD, $DB);
 
     if ($connection->connect_error) {
         die('Connection failed: ' . $connection->connect_error);
